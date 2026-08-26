929eb699cd8244079b2f31d329be1801194f7fa7
wahid23hasan-afk <wahid23hasan@gmail.com>
2026-08-26 13:58:07 +0600

    Post

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/',
+    define: {
+      'process.env': {},
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
